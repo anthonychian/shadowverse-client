c3446bd5272c9fb522a0b306325f0ffed0897383
Anthony C <anthonychian@gmail.com>
2024-01-31 11:42:03 -0800

    changed socket

--- src/sockets.js
+++ src/sockets.js
@@ -1,3 +1,3 @@
 import io from "socket.io-client";
 
-export const socket = io.connect("https://shadowverse-server.vercel.app/");
+export const socket = io.connect("https://shadowverse-server.vercel.app");
