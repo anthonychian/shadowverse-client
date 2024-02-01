e21515ce94a69f4e741258f110a0ae4bce8d085b
Anthony C <anthonychian@gmail.com>
2024-02-01 01:36:54 -0800

    change url

--- src/sockets.js
+++ src/sockets.js
@@ -1,15 +1,17 @@
 import io from "socket.io-client";
 
 // export const socket = io.connect("http://localhost:5000");
-export const socket = io("https://shadowverse-server.vercel.app/", { "transports": ["websocket"]});
+export const socket = io("https://juvenile-closed-stork.glitch.me", {
+  transports: ["websocket"],
+});
 
 socket.on("connect_error", (err) => {
-    // the reason of the error, for example "xhr poll error"
-    console.log(err.message);
+  // the reason of the error, for example "xhr poll error"
+  console.log(err.message);
 
-    // some additional description, for example the status code of the initial HTTP response
-    console.log(err.description);
+  // some additional description, for example the status code of the initial HTTP response
+  console.log(err.description);
 
-    // some additional context, for example the XMLHttpRequest object
-    console.log(err.context);
+  // some additional context, for example the XMLHttpRequest object
+  console.log(err.context);
 });
