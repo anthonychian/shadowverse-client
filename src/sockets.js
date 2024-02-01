02c36e33b6eec733973d7ab4b1d189a4a5c01308
diana <diliang4783573@gmail.com>
2024-01-31 22:17:27 -0800

    add connect error logs

--- src/sockets.js
+++ src/sockets.js
@@ -2,3 +2,14 @@ import io from "socket.io-client";
 
 // export const socket = io.connect("http://localhost:5000");
 export const socket = io("https://shadowverse-server.vercel.app/", { "transports": ["websocket"]});
+
+socket.on("connect_error", (err) => {
+    // the reason of the error, for example "xhr poll error"
+    console.log(err.message);
+
+    // some additional description, for example the status code of the initial HTTP response
+    console.log(err.description);
+
+    // some additional context, for example the XMLHttpRequest object
+    console.log(err.context);
+});
