c20b603bd714d637103783ae5acb2f4b1858daf6
Anthony C <anthonychian@gmail.com>
2024-01-31 17:23:31 -0800

    fix url

--- src/sockets.js
+++ src/sockets.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
 
 // export const socket = io.connect("http://localhost:5000");
-export const socket = io.connect("https://shadowverse-server.vercel.app");
+export const socket = io.connect("http://shadowverse-server.vercel.app");
