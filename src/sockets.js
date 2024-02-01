c65cbba5ebd01111bd92db0a8c117edcb699a11b
diana <diliang4783573@gmail.com>
2024-01-31 22:15:28 -0800

    .

--- src/sockets.js
+++ src/sockets.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
 
 // export const socket = io.connect("http://localhost:5000");
-export const socket = io.connect("https://shadowverse-server.vercel.app/", { "transports": ["websocket"]});
+export const socket = io("https://shadowverse-server.vercel.app/", { "transports": ["websocket"]});
